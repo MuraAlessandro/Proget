930e99ad5562e902d5cb8016961cc36ae116a413
MuraAlessandro <alessandro_1164@alice.it>
2017-02-17 22:05:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/andreacorpino/progetto_ium/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.andreacorpino.progetto_ium;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
